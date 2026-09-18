12da029739165364e67bf1255a55c23787e016e8
romainmannino <romainmannino@gmail.com>
2026-09-18 13:37:21 +0200

    Add app layout

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"QRetail",description:"Le produit physique devient un point de contact digital."};
+export default function RootLayout({children}){return <html lang="fr"><body>{children}</body></html>}
